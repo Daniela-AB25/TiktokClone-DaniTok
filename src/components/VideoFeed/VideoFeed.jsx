7237eb6f2bd92e10177e7217dd19d658f184203e
Daniela-AB25 <25daniela99ab@gmail.com>
2024-06-28 18:15:35 +0200

    VideoFeed started

---
+++ src/components/VideoFeed/VideoFeed.jsx
@@ -0,0 +1,14 @@
+const Videos = [
+  {
+    author: "endlessWaves",
+    src: "https://res.cloudinary.com/dtetsfefb/video/upload/v1719245919/6010489-uhd_2160_3840_25fps_nln6c3.mp4"
+  },
+  {
+    author: "kittiesallaround",
+    src: "https://res.cloudinary.com/dtetsfefb/video/upload/v1719588740/4624594-hd_1080_1920_30fps_eiqjo5.mp4"
+  }
+]
+
+export default funtion VideoFeed() {
+
+}
---
+++ src/components/VideoPlayer/index.jsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react'
+import styles from './styles.module.css'
+import clsx from 'clsx'
+
+
+export default function VideoPlayer({ src }) {
+
+  const [playing, setPlaying] = useState(false)
+  const video = useRef(null)
+
+
+  const handlePlay = () => {
+
+    const { current: videoEl } = video
+    playing
+      ? videoEl.pause()
+      : videoEl.play()
+
+    setPlaying(!playing)
+  }
+
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing,
+  })
+
+  return (
+    <div>
+      <video
+        className={styles.video}
+        src={src}
+        controls={false}
+        ref={video}
+        onClick={handlePlay}
+        loop
+      />
+      <i className={playerClassName} onClick={handlePlay}></i>
+    </div>
+  )
+}
